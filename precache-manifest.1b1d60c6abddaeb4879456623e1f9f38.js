cce64343b4add3823eaf33fdf4a004b68e449b8e
Lucas Padilha <padilhaght@gmail.com>
2020-03-20 00:25:40 +0100

    Updates

---
+++ precache-manifest.1b1d60c6abddaeb4879456623e1f9f38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6db81a0b7543968778c67fe220db621",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a2b6be6417310cdd12e7",
+    "url": "/monsters-rolodex/static/css/main.83bbed23.chunk.css"
+  },
+  {
+    "revision": "4ecb9f0df0474c1b1fa9",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.47062208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2b6be6417310cdd12e7",
+    "url": "/monsters-rolodex/static/js/main.e8f7d578.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
